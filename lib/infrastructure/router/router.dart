468d7246d7c557dbeb98a6d6740a9ff5c284a917
Tiimmy F. Turner <armanaskariofficial@gmail.com>
2025-05-02 12:09:35 +0330

    create HomeScreen (placeholder for router to work)

--- lib/infrastructure/router/router.dart
+++ lib/infrastructure/router/router.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:spot_the_spy/infrastructure/router/router_consts.dart';
+import 'package:spot_the_spy/presentation/screens/home_screen.dart';
 
 class AppRouter {
   static final GoRouter router = GoRouter(
