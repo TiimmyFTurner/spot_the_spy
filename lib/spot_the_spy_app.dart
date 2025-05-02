5831bcb8e1fe3c432d4e91991d473a5317ae3708
Tiimmy F. Turner <armanaskariofficial@gmail.com>
2025-05-02 11:31:43 +0330

    create spy_the_spot_app.dart (set todo for router and localization)

---
+++ lib/spot_the_spy_app.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class SpotTheSpyApp extends ConsumerWidget {
+  const SpotTheSpyApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    /** TODO
+     * Locale locale = ref.watch(localeSettingProvider);
+     * String? font = locale == L10n.fa ? 'Koodak' : null;
+     */
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      //TODO: routerConfig:AppRouter.router,
+      title: 'Spot the Spy',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink),
+        useMaterial3: true,
+        // TODO: fontFamily: font
+      ),
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.pink,
+          brightness: Brightness.dark,
+        ),
+        useMaterial3: true,
+        // TODO: fontFamily: font
+      ),
+      /**TODO:
+       *       themeMode: ref.watch(themeModeSettingProvider),
+       *   supportedLocales: L10n.all,
+       *   locale: locale != L10n.system ? locale : null,
+       *   localizationsDelegates: const [
+       *   AppLocalizations.delegate,
+       *   GlobalCupertinoLocalizations.delegate,
+       *   GlobalMaterialLocalizations.delegate,
+       *   GlobalWidgetsLocalizations.delegate,
+       *  ],
+       *
+       * **/
+    );
+  }
+}
