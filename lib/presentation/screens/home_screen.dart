c74236dd8937e56fb224cb24ab633c5e057cbf08
Tiimmy F. Turner <armanaskariofficial@gmail.com>
2025-05-03 12:48:59 +0330

    create home_screen structure

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -3,6 +3,78 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
+
   @override
-  Widget build(BuildContext context,WidgetRef ref) => const Scaffold(body: Center(child: Text('Home Screen')));
+  Widget build(BuildContext context, WidgetRef ref) => Container(
+    decoration: const BoxDecoration(
+      image: DecorationImage(
+        image: AssetImage("assets/images/wp.png"),
+        fit: BoxFit.cover,
+      ),
+    ),
+    child: Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(35),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              SizedBox(
+                width: double.infinity,
+                height: 60,
+                child: FilledButton(
+                  style: FilledButton.styleFrom(
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(24),
+                        bottom: Radius.circular(4),
+                      ),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Text('New Game', style: TextStyle(fontSize: 24)),
+                ),
+              ),
+              const SizedBox(height: 8),
+              SizedBox(
+                width: double.infinity,
+                height: 60,
+                child: FilledButton.tonal(
+                  style: FilledButton.styleFrom(
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(4),
+                        bottom: Radius.circular(4),
+                      ),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Text('How to play', style: TextStyle(fontSize: 24)),
+                ),
+              ),
+              const SizedBox(height: 8),
+              SizedBox(
+                width: double.infinity,
+                height: 60,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(4),
+                        bottom: Radius.circular(24),
+                      ),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Text('Settings', style: TextStyle(fontSize: 24)),
+                ),
+              ),
+              const SizedBox(height: 50),
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
 }
