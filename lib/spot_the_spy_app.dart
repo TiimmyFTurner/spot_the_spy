c347c7c29f4f84a57d33c2085b73f7c59c7579f5
Tiimmy F. Turner <armanaskariofficial@gmail.com>
2025-05-03 12:59:23 +0330

    fix deprecated synthetic-package for l10n

--- lib/spot_the_spy_app.dart
+++ lib/spot_the_spy_app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:spot_the_spy/infrastructure/router/router.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:spot_the_spy/l10n/app_localizations.dart';
 
 
 
