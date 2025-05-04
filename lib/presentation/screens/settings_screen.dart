3d21d0343cf9d7009557461cbfd1fb4968c503d4
Tiimmy F. Turner <armanaskariofficial@gmail.com>
2025-05-04 22:27:27 +0330

    add copyright and about us message in setting

--- lib/presentation/screens/settings_screen.dart
+++ lib/presentation/screens/settings_screen.dart
@@ -4,6 +4,7 @@ import 'package:go_router/go_router.dart';
 import 'package:spot_the_spy/applications/state_management/settings_provider.dart';
 import 'package:spot_the_spy/l10n/app_localizations.dart';
 import 'package:spot_the_spy/l10n/l10n.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SettingsScreen extends ConsumerStatefulWidget {
   const SettingsScreen({super.key});
@@ -23,54 +24,71 @@ class _SettingsScreenState extends ConsumerState<SettingsScreen> {
       body: Column(
         children: [
           ListTile(
-              title: Text(AppLocalizations.of(context)!.chooseTheme),
-              subtitle: Text(AppLocalizations.of(context)!
-                  .themeMode(ref.watch(themeModeSettingProvider).name)),
-              onTap: () {
-                ThemeMode themeMode = ref.read(themeModeSettingProvider);
-                showDialog<String>(
-                  context: context,
-                  builder: (BuildContext context) =>
-                      StatefulBuilder(builder: (context, setState) {
+            title: Text(AppLocalizations.of(context)!.chooseTheme),
+            subtitle: Text(
+              AppLocalizations.of(
+                context,
+              )!.themeMode(ref.watch(themeModeSettingProvider).name),
+            ),
+            onTap: () {
+              ThemeMode themeMode = ref.read(themeModeSettingProvider);
+              showDialog<String>(
+                context: context,
+                builder:
+                    (BuildContext context) => StatefulBuilder(
+                      builder: (context, setState) {
                         return AlertDialog(
                           contentPadding: EdgeInsets.zero,
-                          title: Text(AppLocalizations.of(context)!.chooseTheme),
-                          content:
-                          Column(mainAxisSize: MainAxisSize.min, children: [
-                            RadioListTile<ThemeMode>(
-                              title: Text(
-                                  AppLocalizations.of(context)!.themeMode('light')),
-                              value: ThemeMode.light,
-                              groupValue: themeMode,
-                              onChanged: (value) {
-                                setState(() {
-                                  themeMode = value!;
-                                });
-                              },
-                            ),
-                            RadioListTile<ThemeMode>(
-                              title: Text(
-                                  AppLocalizations.of(context)!.themeMode('dark')),
-                              value: ThemeMode.dark,
-                              groupValue: themeMode,
-                              onChanged: (value) {
-                                setState(() {
-                                  themeMode = value!;
-                                });
-                              },
-                            ),
-                            RadioListTile<ThemeMode>(
-                              title: Text(AppLocalizations.of(context)!
-                                  .themeMode('system')),
-                              value: ThemeMode.system,
-                              groupValue: themeMode,
-                              onChanged: (value) {
-                                setState(() {
-                                  themeMode = value!;
-                                });
-                              },
-                            ),
-                          ]),
+                          title: Text(
+                            AppLocalizations.of(context)!.chooseTheme,
+                          ),
+                          content: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              RadioListTile<ThemeMode>(
+                                title: Text(
+                                  AppLocalizations.of(
+                                    context,
+                                  )!.themeMode('light'),
+                                ),
+                                value: ThemeMode.light,
+                                groupValue: themeMode,
+                                onChanged: (value) {
+                                  setState(() {
+                                    themeMode = value!;
+                                  });
+                                },
+                              ),
+                              RadioListTile<ThemeMode>(
+                                title: Text(
+                                  AppLocalizations.of(
+                                    context,
+                                  )!.themeMode('dark'),
+                                ),
+                                value: ThemeMode.dark,
+                                groupValue: themeMode,
+                                onChanged: (value) {
+                                  setState(() {
+                                    themeMode = value!;
+                                  });
+                                },
+                              ),
+                              RadioListTile<ThemeMode>(
+                                title: Text(
+                                  AppLocalizations.of(
+                                    context,
+                                  )!.themeMode('system'),
+                                ),
+                                value: ThemeMode.system,
+                                groupValue: themeMode,
+                                onChanged: (value) {
+                                  setState(() {
+                                    themeMode = value!;
+                                  });
+                                },
+                              ),
+                            ],
+                          ),
                           actions: <Widget>[
                             TextButton(
                               onPressed: () => context.pop(),
@@ -83,62 +101,81 @@ class _SettingsScreenState extends ConsumerState<SettingsScreen> {
                                     .read(themeModeSettingProvider.notifier)
                                     .changeTheme(themeMode);
                               },
-                              child: Text(AppLocalizations.of(context)!.confirm),
+                              child: Text(
+                                AppLocalizations.of(context)!.confirm,
+                              ),
                             ),
                           ],
                         );
-                      }),
-                );
-              }),
+                      },
+                    ),
+              );
+            },
+          ),
           ListTile(
-              title: Text(AppLocalizations.of(context)!.language),
-              subtitle: Text(AppLocalizations.of(context)!
-                  .languageMode(ref.watch(localeSettingProvider).toString())),
-              onTap: () {
-                Locale locale = ref.read(localeSettingProvider);
-                showDialog<String>(
-                  context: context,
-                  builder: (BuildContext context) =>
-                      StatefulBuilder(builder: (context, setState) {
+            title: Text(AppLocalizations.of(context)!.language),
+            subtitle: Text(
+              AppLocalizations.of(
+                context,
+              )!.languageMode(ref.watch(localeSettingProvider).toString()),
+            ),
+            onTap: () {
+              Locale locale = ref.read(localeSettingProvider);
+              showDialog<String>(
+                context: context,
+                builder:
+                    (BuildContext context) => StatefulBuilder(
+                      builder: (context, setState) {
                         return AlertDialog(
                           contentPadding: EdgeInsets.zero,
                           title: Text(AppLocalizations.of(context)!.language),
-                          content:
-                          Column(mainAxisSize: MainAxisSize.min, children: [
-                            RadioListTile<Locale>(
-                              title: Text(
-                                  AppLocalizations.of(context)!.languageMode('en')),
-                              value: L10n.en,
-                              groupValue: locale,
-                              onChanged: (value) {
-                                setState(() {
-                                  locale = value!;
-                                });
-                              },
-                            ),
-                            RadioListTile<Locale>(
-                              title: Text(
-                                  AppLocalizations.of(context)!.languageMode('fa')),
-                              value: L10n.fa,
-                              groupValue: locale,
-                              onChanged: (value) {
-                                setState(() {
-                                  locale = value!;
-                                });
-                              },
-                            ),
-                            RadioListTile<Locale>(
-                              title: Text(AppLocalizations.of(context)!
-                                  .themeMode('system')),
-                              value: L10n.system,
-                              groupValue: locale,
-                              onChanged: (value) {
-                                setState(() {
-                                  locale = value!;
-                                });
-                              },
-                            ),
-                          ]),
+                          content: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              RadioListTile<Locale>(
+                                title: Text(
+                                  AppLocalizations.of(
+                                    context,
+                                  )!.languageMode('en'),
+                                ),
+                                value: L10n.en,
+                                groupValue: locale,
+                                onChanged: (value) {
+                                  setState(() {
+                                    locale = value!;
+                                  });
+                                },
+                              ),
+                              RadioListTile<Locale>(
+                                title: Text(
+                                  AppLocalizations.of(
+                                    context,
+                                  )!.languageMode('fa'),
+                                ),
+                                value: L10n.fa,
+                                groupValue: locale,
+                                onChanged: (value) {
+                                  setState(() {
+                                    locale = value!;
+                                  });
+                                },
+                              ),
+                              RadioListTile<Locale>(
+                                title: Text(
+                                  AppLocalizations.of(
+                                    context,
+                                  )!.themeMode('system'),
+                                ),
+                                value: L10n.system,
+                                groupValue: locale,
+                                onChanged: (value) {
+                                  setState(() {
+                                    locale = value!;
+                                  });
+                                },
+                              ),
+                            ],
+                          ),
                           actions: <Widget>[
                             TextButton(
                               onPressed: () => context.pop(),
@@ -151,15 +188,58 @@ class _SettingsScreenState extends ConsumerState<SettingsScreen> {
                                     .read(localeSettingProvider.notifier)
                                     .changeLocale(locale);
                               },
-                              child: Text(AppLocalizations.of(context)!.confirm),
+                              child: Text(
+                                AppLocalizations.of(context)!.confirm,
+                              ),
                             ),
                           ],
                         );
-                      }),
-                );
-              }),
+                      },
+                    ),
+              );
+            },
+          ),
+          const SizedBox(height: 36),
+          const Text(
+            "Spot the Spy 0.0.0",
+            style: TextStyle(color: Colors.grey),
+          ),
+          const Text(
+            "Copyright © 2025 Timothy F. Turner",
+            style: TextStyle(color: Colors.grey),
+          ),
+          InkWell(
+            onTap: _sendMail,
+            child: const Text(
+              "TiimmyFTurner@gmail.com",
+              style: TextStyle(color: Colors.grey),
+            ),
+          ),
+          InkWell(
+            onTap: _openTelegram,
+            child: const Text(
+              "T.me/TiimmyFTurner",
+              style: TextStyle(color: Colors.grey),
+            ),
+          ),
         ],
       ),
     );
+  }
+
+  Future<void> _sendMail() async {
+    final Uri url = Uri.parse(
+      'mailto:TiimmyFTurner@gmail.com?subject=MafiaApp',
+    );
+    if (!await launchUrl(url)) {
+      throw Exception('Could not launch $url');
+    }
+  }
+
+  Future<void> _openTelegram() async {
+    final Uri url = Uri.parse('https://T.me/TiimmyFTurner');
+    if (!await launchUrl(url)) {
+      throw Exception('Could not launch $url');
+    }
   }
 }
