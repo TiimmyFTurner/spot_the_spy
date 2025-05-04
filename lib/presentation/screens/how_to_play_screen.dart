77d846b89840c8a60ae012324f27d79d628d9eea
Tiimmy F. Turner <armanaskariofficial@gmail.com>
2025-05-04 17:24:33 +0330

    create how to play and settings screens

---
+++ lib/presentation/screens/how_to_play_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class HowToPlayScreen extends ConsumerStatefulWidget {
+  const HowToPlayScreen({super.key});
+
+  @override
+  ConsumerState createState() => _HowToPlayScreenState();
+}
+
+class _HowToPlayScreenState extends ConsumerState<HowToPlayScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
