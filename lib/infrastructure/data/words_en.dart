be5484d70925d4f54de5c64501c6b30fdaeb9bdd
Tiimmy F. Turner <armanaskariofficial@gmail.com>
2025-05-05 18:08:56 +0330

    create word lists and categoery map

---
+++ lib/infrastructure/data/words_en.dart
@@ -0,0 +1,204 @@
+final List<String> animals = const [
+  'Lion', 'Tiger', 'Elephant', 'Giraffe', 'Zebra', 'Kangaroo', 'Panda',
+  'Wolf', 'Fox', 'Rabbit', 'Bear', 'Monkey', 'Horse', 'Deer', 'Dog', 'Cat',
+  'Turtle', 'Crocodile', 'Dolphin', 'Owl', 'Snake', 'Mouse', 'Eagle',
+  'Frog', 'Penguin', 'Whale', 'Bat', 'Ant', 'Bee', 'Lizard'
+];
+
+final List<String> cities = const [
+  'Paris', 'London', 'Tokyo', 'New York', 'Rome', 'Sydney', 'Cairo',
+  'Moscow', 'Toronto', 'Dubai', 'Berlin', 'Madrid', 'Beijing', 'Seoul',
+  'Rio', 'Vienna', 'Bangkok', 'Los Angeles', 'Istanbul', 'Amsterdam',
+  'Chicago', 'San Francisco', 'Lima', 'Mumbai', 'Johannesburg', 'Lisbon',
+  'Stockholm', 'Hanoi', 'Prague', 'Oslo'
+];
+
+final List<String> foodAndDrinks = const [
+  'Pizza', 'Burger', 'Pasta', 'Sushi', 'Steak', 'Salad', 'Fries',
+  'Coffee', 'Tea', 'Juice', 'Coke', 'Water', 'Ice Cream', 'Chocolate',
+  'Soup', 'Sandwich', 'Cake', 'Milk', 'Bread', 'Rice', 'Noodles', 'Cheese',
+  'Hotdog', 'Yogurt', 'Donut', 'Orange', 'Banana', 'Apple', 'Eggs', 'Chips'
+];
+
+final List<String> jobs = const [
+  'Doctor', 'Engineer', 'Teacher', 'Chef', 'Pilot', 'Nurse', 'Police',
+  'Artist', 'Lawyer', 'Farmer', 'Firefighter', 'Mechanic', 'Scientist',
+  'Journalist', 'Carpenter', 'Dentist', 'Musician', 'Librarian', 'Driver',
+  'Plumber', 'Architect', 'Electrician', 'Actor', 'Cashier', 'Designer',
+  'Photographer', 'Baker', 'Paramedic', 'Technician', 'Receptionist'
+];
+
+final List<String> sports = const [
+  'Soccer', 'Basketball', 'Tennis', 'Baseball', 'Swimming', 'Volleyball',
+  'Boxing', 'Golf', 'Skiing', 'Skating', 'Rugby', 'Cricket', 'Surfing',
+  'Wrestling', 'Cycling', 'Hockey', 'Gymnastics', 'Karate', 'Rowing',
+  'Bowling', 'Judo', 'Fencing', 'Climbing', 'Running', 'Badminton',
+  'Table Tennis', 'Snooker', 'Snowboarding', 'Skateboarding', 'Diving'
+];
+
+final List<String> school = const [
+  'Classroom', 'Desk', 'Teacher', 'Student', 'Book', 'Pencil', 'Notebook', 'Eraser', 'Test', 'Exam',
+  'Homework', 'Ruler', 'Glue', 'Backpack', 'Lunchbox', 'Principal', 'Bell', 'Marker', 'Scissors',
+  'Blackboard', 'Chair', 'Board', 'Map', 'Pen', 'Paper', 'Folder', 'Highlighter', 'Calculator',
+  'Chalk', 'Clock'
+];
+
+final List<String> movies = const [
+  'Titanic', 'Avatar', 'Inception', 'Frozen', 'Batman', 'Avengers', 'Matrix', 'Toy Story',
+  'Nemo', 'Shrek', 'Iron Man', 'Harry Potter', 'Deadpool', 'Dune', 'Coco', 'Up', 'Minions',
+  'Gladiator', 'Joker', 'Aladdin', 'Moana', 'Cars', 'Spider-Man', 'Star Wars', 'Rocky',
+  'Interstellar', 'The Godfather', 'The Lion King', 'Black Panther', 'The Notebook'
+];
+
+final List<String> famousPeople = const [
+  'Einstein', 'Shakespeare', 'Elvis', 'Oprah', 'Obama', 'Tesla', 'Beethoven', 'Newton', 'Mandela',
+  'Mozart', 'Da Vinci', 'Cleopatra', 'Churchill', 'Gandhi', 'Steve Jobs', 'Freud', 'Elon Musk',
+  'Taylor Swift', 'Ronaldo', 'Beyoncé', 'Trump', 'Hitler', 'Lincoln', 'Zuckerberg', 'Putin',
+  'Messi', 'Drake', 'Einstein', 'Napoleon', 'Marie Curie' // Note: Einstein appears twice
+];
+
+final List<String> brands = const [
+  'Apple', 'Nike', 'Coca-Cola', 'Samsung', 'Adidas', 'Google', 'Microsoft', 'McDonald\'s', // Escaped apostrophe is fine, or use "McDonald's"
+  'Amazon', 'Pepsi', 'Toyota', 'BMW', 'Sony', 'IKEA', 'Netflix', 'Disney', 'Starbucks',
+  'Lego', 'Intel', 'Gucci', 'Audi', 'Honda', 'Dell', 'Ford', 'KFC', 'YouTube',
+  'Instagram', 'Snapchat', 'Zara', 'H&M'
+];
+
+final List<String> nature = const [
+  'Tree', 'River', 'Mountain', 'Ocean', 'Lake', 'Flower', 'Rain', 'Snow', 'Sun', 'Moon',
+  'Rock', 'Leaf', 'Cloud', 'Volcano', 'Wind', 'Cave', 'Waterfall', 'Beach', 'Desert', 'Forest',
+  'Star', 'Sky', 'Storm', 'Grass', 'Pond', 'Valley', 'Hill', 'Sand', 'Island', 'Lightning'
+];
+
+final List<String> clothing = const [
+  'Shirt', 'Pants', 'Shoes', 'Socks', 'Hat', 'Jacket', 'Sweater', 'Dress', 'Tie', 'Gloves',
+  'Scarf', 'Belt', 'Boots', 'Skirt', 'Jeans', 'Shorts', 'Coat', 'Watch', 'Sunglasses', 'Cap',
+  'Suit', 'Blazer', 'Hoodie', 'Tank Top', 'Raincoat', 'Vest', 'Overalls', 'Mittens', 'Sandals', 'Heels'
+];
+
+final List<String> hobbies = const [
+  'Reading', 'Drawing', 'Cooking', 'Gardening', 'Photography', 'Dancing', 'Singing', 'Fishing', 'Hiking',
+  'Writing', 'Painting', 'Gaming', 'Knitting', 'Traveling', 'Origami', 'Skating', 'Cycling', 'Swimming',
+  'Camping', 'Woodworking', 'Baking', 'Sculpting', 'Jogging', 'Yoga', 'Birdwatching', 'Chess',
+  'Collecting', 'Sewing', 'Archery', 'Puzzles'
+];
+
+final List<String> videoGames = const [
+  'Minecraft', 'Fortnite', 'Zelda', 'Mario', 'Call of Duty', 'Halo', 'Among Us', 'Pokemon', 'Tetris',
+  'Sonic', 'Pac-Man', 'Overwatch', 'League of Legends', 'GTA', 'Roblox', 'FIFA', 'Animal Crossing',
+  'God of War', 'Apex Legends', 'Elden Ring', 'PUBG', 'Valorant', 'Counter-Strike', 'The Sims',
+  'Doom', 'Metroid', 'StarCraft', 'Fall Guys', 'Terraria', 'Dota'
+];
+
+final List<String> weather = const [
+  'Rain', 'Snow', 'Sun', 'Clouds', 'Storm', 'Thunder', 'Lightning', 'Wind', 'Fog', 'Hail',
+  'Drizzle', 'Heatwave', 'Tornado', 'Hurricane', 'Blizzard', 'Rainbow', 'Chilly', 'Humid',
+  'Freezing', 'Drought', 'Mist', 'Sunshine', 'Frost', 'Icicle', 'Overcast', 'Cloudburst',
+  'Typhoon', 'Downpour', 'Dust Storm', 'Gale'
+];
+
+final List<String> tools = const [
+  'Hammer', 'Screwdriver', 'Wrench', 'Pliers', 'Saw', 'Drill', 'Tape Measure', 'Level', 'Axe',
+  'Chisel', 'Clamp', 'File', 'Ladder', 'Shovel', 'Sander', 'Toolbox', 'Flashlight', 'Crowbar',
+  'Nails', 'Screws', 'Allen Key', 'Socket Wrench', 'Gloves', 'Welding Mask', 'Workbench',
+  'Vice', 'Measuring Tape', 'Brush', 'Trowel', 'Sandpaper'
+];
+
+final List<String> transportation = const [
+  'Car', 'Bus', 'Bicycle', 'Motorcycle', 'Train', 'Airplane', 'Helicopter', 'Boat', 'Subway',
+  'Taxi', 'Scooter', 'Truck', 'Skateboard', 'Ship', 'Jet', 'Yacht', 'Tram', 'Van', 'Cable Car',
+  'Rickshaw', 'Ferry', 'Segway', 'Hoverboard', 'Hot Air Balloon', 'Monorail', 'Ambulance',
+  'Fire Truck', 'Police Car', 'Bulldozer', 'Tank'
+];
+
+final List<String> householdItems = const [
+  'Chair', 'Table', 'Lamp', 'Couch', 'Spoon', 'Fork', 'Knife', 'Cup', 'Plate', 'Mirror',
+  'Bed', 'Pillow', 'Blanket', 'Toothbrush', 'Shampoo', 'Towel', 'Fridge', 'Oven', 'Microwave',
+  'Television', 'Fan', 'Clock', 'Vacuum', 'Curtains', 'Rug', 'Remote', 'Desk', 'Washer',
+  'Dryer', 'Trash Can'
+];
+
+final List<String> emotions = const [
+  'Happy', 'Sad', 'Angry', 'Excited', 'Bored', 'Surprised', 'Scared', 'Nervous', 'Confused',
+  'Proud', 'Jealous', 'Lonely', 'Frustrated', 'Embarrassed', 'Grateful', 'Shy', 'Tired',
+  'Annoyed', 'Hopeful', 'Disappointed', 'Joyful', 'Relaxed', 'Curious', 'Ashamed',
+  'Worried', 'Eager', 'Loving', 'Silly', 'Peaceful', 'Upset'
+];
+
+final List<String> colors = const [
+  'Red', 'Blue', 'Green', 'Yellow', 'Orange', 'Purple', 'Pink', 'Brown', 'Black', 'White',
+  'Gray', 'Cyan', 'Magenta', 'Lime', 'Olive', 'Navy', 'Beige', 'Maroon', 'Coral', 'Teal',
+  'Turquoise', 'Gold', 'Silver', 'Lavender', 'Ivory', 'Charcoal', 'Peach', 'Mint', 'Crimson', 'Amber'
+];
+
+final List<String> countries = const [
+  'USA', 'Canada', 'Mexico', 'Brazil', 'Argentina', 'UK', 'France', 'Germany', 'Italy',
+  'Spain', 'Russia', 'China', 'Japan', 'India', 'Australia', 'Egypt', 'South Africa',
+  'Turkey', 'Iran', 'Saudi Arabia', 'Thailand', 'Vietnam', 'South Korea', 'Indonesia',
+  'Sweden', 'Norway', 'Greece', 'Ukraine', 'Poland', 'Pakistan'
+];
+
+final List<String> bodyParts = const [
+  'Head', 'Eye', 'Nose', 'Mouth', 'Ear', 'Arm', 'Leg', 'Hand', 'Foot', 'Finger',
+  'Toe', 'Back', 'Shoulder', 'Knee', 'Elbow', 'Hair', 'Neck', 'Chin', 'Cheek',
+  'Belly', 'Heart', 'Lungs', 'Liver', 'Nails', 'Wrist', 'Ankle', 'Thigh', 'Heel', 'Thumb', 'Tongue'
+];
+
+final List<String> musicalInstruments = const [
+  'Guitar', 'Piano', 'Drum', 'Violin', 'Flute', 'Trumpet', 'Saxophone', 'Cello',
+  'Clarinet', 'Trombone', 'Harp', 'Accordion', 'Tambourine', 'Triangle', 'Maracas',
+  'Xylophone', 'Bongo', 'Tuba', 'Organ', 'Mandolin', 'Banjo', 'Oboe', 'Synthesizer',
+  'Ukulele', 'Bass', 'Recorder', 'Castanets', 'Congas', 'Cymbals', 'Kalimba'
+];
+
+final List<String> space = const [
+  'Planet', 'Star', 'Moon', 'Sun', 'Asteroid', 'Comet', 'Meteor', 'Galaxy', 'Black Hole',
+  'Nebula', 'Satellite', 'Rocket', 'Spaceship', 'Astronaut', 'Mars', 'Venus', 'Jupiter',
+  'Saturn', 'Uranus', 'Neptune', 'Earth', 'Telescope', 'Space Station', 'Milky Way',
+  'Orbit', 'Gravity', 'Solar System', 'Cosmos', 'Alien', 'Crater'
+];
+
+final List<String> furniture = const [
+  'Chair', 'Table', 'Bed', 'Sofa', 'Desk', 'Bookshelf', 'Cabinet', 'Dresser', 'Wardrobe',
+  'Couch', 'Stool', 'Bench', 'Coffee Table', 'Nightstand', 'Armchair', 'Closet', 'Shelf',
+  'Drawer', 'TV Stand', 'Crib', 'Recliner', 'Ottoman', 'Mirror', 'Side Table', 'Vanity',
+  'Futon', 'Chest', 'Divan', 'Bar Stool', 'Rocking Chair'
+];
+
+final List<String> technology = const [
+  'Phone', 'Laptop', 'Computer', 'Tablet', 'Charger', 'Cable', 'Monitor', 'Mouse',
+  'Keyboard', 'Headphones', 'Camera', 'Drone', 'USB', 'Router', 'Printer', 'Smartwatch',
+  'Speaker', 'TV', 'Remote', 'Microphone', 'Projector', 'Battery', 'Hard Drive',
+  'Flash Drive', 'Scanner', 'Webcam', 'Game Console', 'Wi-Fi', 'Bluetooth', 'Smart Home'
+];
+
+final List<String> drinks = const [
+  'Water', 'Tea', 'Coffee', 'Juice', 'Milk', 'Cola', 'Lemonade', 'Smoothie', 'Soda',
+  'Beer', 'Wine', 'Whiskey', 'Vodka', 'Champagne', 'Hot Chocolate', 'Milkshake',
+  'Espresso', 'Latte', 'Mocha', 'Iced Tea', 'Energy Drink', 'Cocktail', 'Herbal Tea',
+  'Green Tea', 'Cider', 'Tonic Water', 'Orange Juice', 'Apple Juice', 'Coconut Water', 'Soy Milk'
+];
+
+final List<String> vehicles = const [
+  'Car', 'Truck', 'Bus', 'Bike', 'Motorcycle', 'Boat', 'Jet', 'Submarine', 'Scooter',
+  'Train', 'Helicopter', 'Plane', 'Tractor', 'Bulldozer', 'Ambulance', 'Fire Truck',
+  'Police Car', 'Tank', 'Golf Cart', 'Spaceship', 'Cable Car', 'ATV', 'Van', 'Rickshaw',
+  'Raft', 'Yacht', 'Ferry', 'Skateboard', 'Glider', 'Snowmobile'
+];
+
+final List<String> electronics = const [
+  'Smartphone', 'Laptop', 'Tablet', 'TV', 'Remote', 'Smartwatch', 'Camera', 'Drone',
+  'Microwave', 'Fridge', 'Washing Machine', 'Fan', 'Router', 'Projector', 'Printer',
+  'Monitor', 'Speaker', 'Game Console', 'Keyboard', 'Mouse', 'Headphones', 'VR Headset',
+  'Flash Drive', 'Battery', 'Calculator', 'Clock', 'Hair Dryer', 'Electric Toothbrush',
+  'Radio', 'Alarm Clock'
+];
+
+final List<String> holidays = const [
+  'Christmas', 'New Year', 'Easter', 'Halloween', 'Thanksgiving', 'Valentine\'s Day',
+  'Independence Day', 'Hanukkah', 'Diwali', 'Ramadan', 'Nowruz', 'Chinese New Year',
+  'Labor Day', 'Mother\'s Day', 'Father\'s Day', 'Earth Day', 'April Fools\' Day',
+  'Veterans Day', 'Memorial Day', 'St. Patrick\'s Day', 'Boxing Day', 'Carnival',
+  'Black Friday', 'Eid al-Fitr', 'Eid al-Adha', 'Rosh Hashanah', 'Yom Kippur',
+  'Good Friday', 'International Women\'s Day', 'Arbor Day'
+];
